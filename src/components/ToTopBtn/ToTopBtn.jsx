9335b95bb43fd045f126e17dca6bc07fbc068cb1
Volodymyr Shum <shum.vr@gmail.com>
2022-04-28 13:23:58 +0300

    Add scroll to top button

--- src/components/ToTopBtn/ToTopBtn.jsx
+++ src/components/ToTopBtn/ToTopBtn.jsx
@@ -1,10 +1,35 @@
+import { useState } from 'react';
+
 import toTop from '../../assets/icons/toTop.svg';
 
 import styles from './ToTopBtn.module.scss';
 
 export const ToTopBtn = () => {
+	const [visible, setVisible] = useState(false);
+
+	const toggleVisible = () => {
+		const scrolled = document.documentElement.scrollTop;
+		if (scrolled > 200) {
+			setVisible(true);
+		} else if (scrolled <= 200) {
+			setVisible(false);
+		}
+	};
+
+	const scrollToTop = () => {
+		window.scrollTo({
+			top: 0,
+			behavior: 'smooth',
+		});
+	};
+
+	window.addEventListener('scroll', toggleVisible);
 	return (
-		<div className={styles.toTop}>
+		<div
+			className={styles.toTop}
+			onClick={scrollToTop}
+			style={{ display: visible ? 'inline' : 'none' }}
+		>
 			<a href='#header'>
 				<img src={toTop} alt='toTop' />
 			</a>
